deae99f30e25fb6840ed704fcceee78359efa1b6
xistne <39307755+xistne@users.noreply.github.com>
2023-12-23 17:48:26 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
@@ -1,9 +1,10 @@
+dp = [0] * 12;
+dp[1] = 1;
+dp[2] = 2;
+dp[3] = 4;
+for i in range(4, 12) :
+  dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
+
 for i in range(int(input())) :
   n = int(input());
-  dp = [0] * 12;
-  dp[1] = 1;
-  dp[2] = 2;
-  dp[3] = 4;
-  for i in range(4, n + 1) :
-    dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
   print(dp[n]);
