1081921a309229373361480dbf9e69c1a87f4faf
xistne <39307755+xistne@users.noreply.github.com>
2023-12-31 03:07:25 +0900

    [Silver II] Title: 연속합, Time: 96 ms, Memory: 38828 KB -BaekjoonHub

--- 백준/Silver/1912. 연속합/연속합.py
+++ 백준/Silver/1912. 연속합/연속합.py
@@ -1,8 +1,7 @@
 n = int(input());
-arr = list(map(int,input().split()));
-dp = [0] * n;
-dp[0] = arr[0];
-for i in range(1,n):
-  dp[i] = max(dp[i-1],arr[i-1],0) + arr[i];
-
-print(max(dp));
+m = list( map(int, input().split()));
+ 
+for i in range(1, n):
+    m[i] = max(m[i], m[i] + m[i-1]);
+    
+print(max(m));
