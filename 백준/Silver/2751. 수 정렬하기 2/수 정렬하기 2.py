c824e2442cbc0b284219e45bbd44d0f90000f406
xistne <39307755+xistne@users.noreply.github.com>
2024-01-06 01:47:15 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1316 ms, Memory: 76972 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.py
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.py
@@ -1,39 +1,8 @@
 import sys
 input=sys.stdin.readline
 
-def mergesplit(data) :
-  if len(data) <= 1:
-    return data;
-  medium = int(len(data) // 2);
-  left = mergesplit(data[:medium]);
-  right = mergesplit(data[medium:]);
-
-  return merge(left,right);
-
-def merge(left,right):
-  merged = list();
-  left_point, right_point = 0, 0;
-
-  while left_point < len(left) and right_point < len(right):
-    if left[left_point] < right[right_point]:
-      merged.append(left[left_point]);
-      left_point +=1;
-    else:
-      merged.append(right[right_point]);
-      right_point +=1;
-
-  while left_point < len(left):
-    merged.append(left[left_point]);
-    left_point +=1;
-
-  while right_point < len(right):
-    merged.append(right[right_point]);
-    right_point +=1;
-
-  return merged;
-
 n = int(input());
 data = [int(input()) for _ in range(n)];
-sortedlist = mergesplit(data);
+data.sort();
 for i in range(n):
-    print(sortedlist[i]);
+    print(data[i]);
