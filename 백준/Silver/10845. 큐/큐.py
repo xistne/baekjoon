2a6693421db0f5735242a328a6cace0b33f0a863
xistne <39307755+xistne@users.noreply.github.com>
2024-01-15 19:36:03 +0900

    [Silver IV] Title: 큐, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.py
@@ -0,0 +1,19 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input());
+arr = [];
+for i in range(n):
+  k = input().replace("\n", "").split(" ");
+  if k[0] == 'push':
+    arr.append(k[1]);
+  elif k[0] == 'pop':
+    print( -1 if len(arr) == 0 else arr.pop(0));
+  elif k[0] == 'size':
+    print(len(arr));
+  elif k[0] == 'empty':
+    print( 1 if len(arr) == 0 else 0);
+  elif k[0] == 'front':
+    print( arr[0] if len(arr) != 0 else -1);
+  elif k[0] == 'back':
+    print( arr[-1] if len(arr) != 0 else -1);
