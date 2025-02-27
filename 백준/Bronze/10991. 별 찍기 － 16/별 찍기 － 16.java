57916ad6eda31a8cb9a3a1bbb7fe2a1bc9cf4891
xistne <39307755+xistne@users.noreply.github.com>
2025-02-28 01:23:59 +0900

    [Bronze III] Title: 별 찍기 - 16, Time: 112 ms, Memory: 14484 KB -BaekjoonHub

---
+++ 백준/Bronze/10991. 별 찍기 － 16/별 찍기 － 16.java
@@ -0,0 +1,15 @@
+    import java.io.BufferedReader;
+    import java.io.IOException;
+    import java.io.InputStreamReader;
+    public class Main {
+        public static void main(String[] args) throws IOException {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+            int n = Integer.parseInt(br.readLine());
+
+            for (int i = 1; i < n+1; i++) {
+                System.out.print(" ".repeat(n-i));
+                System.out.println("* ".repeat(i));
+            }
+        }
+    }
