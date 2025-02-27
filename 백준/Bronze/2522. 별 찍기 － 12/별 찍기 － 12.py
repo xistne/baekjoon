7a1ed87b77a558cfadffcceec70d8a04160c09ef
xistne <39307755+xistne@users.noreply.github.com>
2025-02-28 01:09:17 +0900

    [Bronze III] Title: 별 찍기 - 12, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2522. 별 찍기 － 12/별 찍기 － 12.py
+++ 백준/Bronze/2522. 별 찍기 － 12/별 찍기 － 12.py
@@ -1,6 +1,9 @@
-n = int(input());
+n = int(input())
+
+for i in range(1,n+1):
+  print(" "*(n-i),end="")
+  print("*"*i)
 
 for i in range(1,n):
-    print(' '*(n-i)+'*'*i);
-for i in range(n,0,-1):
-    print(' '*(n-i)+'*'*i);
+  print(" "*(i),end="")
+  print("*"*(n-i))
