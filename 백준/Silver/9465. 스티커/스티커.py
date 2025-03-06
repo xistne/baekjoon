0070ed217e0b5ec57d5f9601797fd74ea6f92ae7
xistne <39307755+xistne@users.noreply.github.com>
2025-03-06 15:00:50 +0900

    [Silver I] Title: 스티커, Time: 640 ms, Memory: 49684 KB -BaekjoonHub

--- 백준/Silver/9465. 스티커/스티커.py
+++ 백준/Silver/9465. 스티커/스티커.py
@@ -1,15 +1,15 @@
-n = int(input());
+t = int(input())
 
-for i in range(n):
-  m = int(input());
-  dp = [list(map(int,input().split())) for _ in range(2)];
-  
-  if m > 1 :
-    dp[0][1] += dp[1][0];
-    dp[1][1] += dp[0][0];
+for i in range(t):
+  n = int(input())
+  dp = [[0] * n for _ in range(2)]
+  arr = [list(map(int,input().split())) for _ in range(2)]
+  for j in range(1,n):
+    if j == 1:
+      arr[0][j] += arr[1][j-1]
+      arr[1][j] += arr[0][j-1]
+    else:
+      arr[0][j] += max(arr[1][j-1],arr[1][j-2])
+      arr[1][j] += max(arr[0][j-1],arr[0][j-2])
 
-  for j in range(2,m):
-    dp[0][j] += max(dp[1][j-1],dp[1][j-2]);
-    dp[1][j] += max(dp[0][j-1],dp[0][j-2]);
-
-  print(max(dp[0][m-1],dp[1][m-1]));
+  print(max(map(max,arr)))
