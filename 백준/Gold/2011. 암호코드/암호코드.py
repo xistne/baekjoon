c309edc6123aba658dd972518adf5e545b4991b4
xistne <39307755+xistne@users.noreply.github.com>
2025-03-09 12:29:26 +0900

    [Gold V] Title: 암호코드, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/2011. 암호코드/암호코드.py
+++ 백준/Gold/2011. 암호코드/암호코드.py
@@ -1,18 +1,16 @@
-arr = list(map(int,input()));
-n = len(arr);
-dp = [0] * (n+1);
+arr = [0]+list(map(int,input()))
+n = len(arr)
+dp = [0] * (n)
 dp[0] = dp[1] = 1
+if arr[1] == 0 :
+    print(0)
+    exit(0)
 
-if arr[0] == 0:
-  print(0);
-  exit(0);
-  
-for i in range(2,n+1):
-  if arr[i-1] > 0:
-    dp[i] += dp[i-1];
-
-  temp = arr[i-2] * 10 + arr[i-1];
-  if temp >= 10 and temp <= 26:
-    dp[i] += dp[i-2];
+for i in range(2, n):
+    if arr[i] > 0 :
+        dp[i] = dp[i-1]
     
-print(dp[n] % 1000000);
+    if 10 <= arr[i] + arr[i-1]*10 < 27 :
+        dp[i] += dp[i-2]
+
+print(dp[n-1]%1000000)
