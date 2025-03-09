4f7a99b781e4ca77cf3a6e5804fa44d42e8ab958
xistne <39307755+xistne@users.noreply.github.com>
2025-03-09 14:17:58 +0900

    [Silver I] Title: 카드 구매하기, Time: 172 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/11052. 카드 구매하기/카드 구매하기.py
+++ 백준/Silver/11052. 카드 구매하기/카드 구매하기.py
@@ -1,10 +1,9 @@
-n = int(input());
-arr = list(map(int,input().split()));
-arr.insert(0,0);
-dp = [0] * (n+1);
+n = int(input())
+arr = [0]+list(map(int,input().split()))
+dp = arr[:]
+
 
 for i in range(1,n+1):
-  for j in range(1,i+1):
-      dp[i] = max(dp[i],dp[i-j] + arr[j])
-
-print(dp[n]);
+    for j in range(1,i):
+        dp[i] = max(dp[i],dp[j]+dp[i-j])
+print(dp[n])
