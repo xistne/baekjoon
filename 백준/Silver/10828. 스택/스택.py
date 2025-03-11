ec6dace91dba288844d5cfc6fb7ee6aa08463dd3
xistne <39307755+xistne@users.noreply.github.com>
2025-03-11 12:54:45 +0900

    [Silver IV] Title: 스택, Time: 44 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.py
+++ 백준/Silver/10828. 스택/스택.py
@@ -1,28 +1,16 @@
 import sys
-input = sys.stdin.readline
-
-n = int(input());
-arr = [];
+n = int(input())
+arr = []
 for i in range(n):
-  data = input().replace("\n","");
-  dataList = data.split(' ');
-  if dataList[0] == "push":
-    arr.append(dataList[1]);
-  elif dataList[0] == "pop":
-    if(len(arr) ==0):
-      print(-1);
-      continue;
-    popped = arr.pop();
-    print(popped);
-  elif dataList[0] == "size":
-    print(len(arr));
-  elif dataList[0] == "empty":
-    if len(arr) == 0:
-      print(1);
-      continue;
-    print(0);
-  elif dataList[0] == "top":
-    if len(arr) == 0:
-      print(-1);
-      continue;
-    print(arr[-1]);
+    command = sys.stdin.readline().split()
+    if command[0] == 'push':
+        arr.append(command[1])
+    elif command[0] == 'pop':
+        print(arr.pop() if len(arr) != 0 else -1)
+    elif command[0] == 'size':
+        print(len(arr))
+    elif command[0] == 'empty':
+        print(1 if len(arr) == 0 else 0)
+    elif command[0] == 'top':
+        print(arr[-1] if len(arr) != 0 else -1)
+
