63e3876ece013e52359ff156829df351926dbcda
xistne <39307755+xistne@users.noreply.github.com>
2025-03-23 20:59:17 +0900

    [Silver IV] Title: 괄호, Time: 36 ms, Memory: 32544 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.py
+++ 백준/Silver/9012. 괄호/괄호.py
@@ -1,18 +1,23 @@
-n = int(input());
+import sys
+input = sys.stdin.readline
+n = int(input())
+
 for i in range(n):
-  data = list(input());
-  arr = [];
-  for i in range(len(data)):
-    if data[i] == '(':
-      arr.append(data[i]);
+    arr_origin = list(input().strip())
+    arr_stack = []
+    flag = True
+
+    for i in arr_origin:
+        if '(' == i :
+            arr_stack.append(i)
+        elif ')' == i :
+            if len(arr_stack) == 0:
+                flag = False
+                break
+            else :
+                arr_stack.pop()
+
+    if flag == False or len(arr_stack) != 0:
+        print("NO")
     else:
-      if len(arr) == 0:
-        print('NO');
-        break;
-      else : 
-        arr.pop();
-    if i == len(data)-1:
-      if len(arr) == 0 :
-        print('YES');
-      else :
-        print('NO');
+        print("YES")
